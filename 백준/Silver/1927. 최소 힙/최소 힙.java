165b581b41b351cb0d31e06bd44498853840d138
taejeongH <taejeongh98@gmail.com>
2025-07-20 10:00:15 +0900

    [Silver II] Title: 최소 힙, Time: 316 ms, Memory: 26644 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {	
+	public static void main(String[] ars) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(br.readLine());
+			if (num != 0) {
+				pq.add(num);
+			} else {
+				if (!pq.isEmpty()) {
+					bw.write(String.valueOf(pq.poll()) + "\n");
+				} else {
+					bw.write("0" + "\n");
+				}
+				
+			}
+		}
+		
+		bw.flush();
+		bw.close();
+		
+	}
+
+}
