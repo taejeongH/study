b7ff07c203bcf3552d2738e47bea6b58ff45b0f6
taejeongH <taejeongh98@gmail.com>
2025-09-08 16:37:49 +0900

    [Gold V] Title: 퇴사 2, Time: 764 ms, Memory: 339748 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -9,29 +9,25 @@ public class Main {
 		StringTokenizer st = null;
 		int N = Integer.parseInt(br.readLine());
 		
-		int[][] map = new int[N][2];
-		List<int[]>[] g = new List[N+1]; for (int i=0; i<N+1; i++) g[i] = new ArrayList<>();
+		int[][] map = new int[N+1][2];
 		for (int i=1; i<N+1; i++) {
 			st = new StringTokenizer(br.readLine());
-			int start = i;
-			int end = start+Integer.parseInt(st.nextToken())-1;
-			int price = Integer.parseInt(st.nextToken());
-			if(end>N) continue; 
-			g[end].add(new int[] {start, price});
+			map[i][0] = Integer.parseInt(st.nextToken());
+			map[i][1] = Integer.parseInt(st.nextToken());
 		}
 		
 		int[] dp = new int[N+1];
 		int result = 0;
 		for (int i=1; i<N+1; i++) {
-			dp[i] = dp[i-1];
-			for (int j=0; j<g[i].size(); j++) {
-				int curStart = g[i].get(j)[0];
-				dp[i] = Math.max(dp[curStart-1]+g[i].get(j)[1], dp[i]);
+			int end = i + map[i][0] - 1;
+			dp[i]=Math.max(dp[i], dp[i-1]);
+			if(end <= N) {
+				dp[end] = Math.max(dp[end], dp[i-1]+map[i][1]);
 			}
-			result = Math.max(dp[i], result);
+//			result = Math.max(dp[i], result);
 			
 		}
 //		System.out.println(Arrays.toString(dp));
-		System.out.println(result);
+		System.out.println(dp[N]);
 	}
 }
