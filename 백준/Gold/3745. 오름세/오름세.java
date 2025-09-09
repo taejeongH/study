b2d4b58fabfa4109f1a19ca2316721a72a17a753
taejeongH <taejeongh98@gmail.com>
2025-09-09 11:20:17 +0900

    [Gold II] Title: 오름세, Time: 284 ms, Memory: 27712 KB -BaekjoonHub

--- 백준/Gold/3745. 오름세/오름세.java
+++ 백준/Gold/3745. 오름세/오름세.java
@@ -5,16 +5,19 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		Scanner sc = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		
-		while(sc.hasNext()) {
-			int N = sc.nextInt();
+		
+		String input = "";
+		while((input = br.readLine()) != null) {
+			int N = Integer.parseInt(input.trim());
 			int[] map = new int[N];
 			int[] L = new int[N];
 			int max = 0;
+			StringTokenizer st = new StringTokenizer(br.readLine());
 			for (int i=0; i<N; i++) {
-				map[i] = sc.nextInt();
+				map[i] = Integer.parseInt(st.nextToken());
 				int pos = Arrays.binarySearch(L,  0, max, map[i]); 
 				if(pos<0) pos=-(pos+1);
 				L[pos]=map[i];
