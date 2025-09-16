d5ec516ee1562658d042932c2e2ecc83966be170
taejeongH <taejeongh98@gmail.com>
2025-09-16 22:16:21 +0900

    [Gold IV] Title: 운동, Time: 508 ms, Memory: 58576 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.java
+++ 백준/Gold/1956. 운동/운동.java
@@ -4,49 +4,47 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	static final int INF = 1_000_000_000;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
 		int V = Integer.parseInt(st.nextToken());
 		int E = Integer.parseInt(st.nextToken());
-		List<int[]>[] g = new List[V+1]; for(int i=1; i<V+1; i++) g[i] = new ArrayList<>();
+		int[][] g = new int[V+1][V+1];
+		for (int i=1; i<V+1; i++) Arrays.fill(g[i], INF);
 		for (int i=0; i<E; i++) {
 			st = new StringTokenizer(br.readLine());
 			int s = Integer.parseInt(st.nextToken());
 			int e = Integer.parseInt(st.nextToken());
 			int c = Integer.parseInt(st.nextToken());
-			g[s].add(new int[] {e, c});
+			g[s][e] = c;
 		}
 		
-		int res = Integer.MAX_VALUE;
-		for (int i=1; i<V+1; i++) {
-			PriorityQueue<int[]> que = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[1], o2[1]));
-			int[] distance = new int[V+1];
-			Arrays.fill(distance, Integer.MAX_VALUE);
-			que.add(new int[] {i, 0});
-			while(!que.isEmpty()) {
-				int[] now = que.poll();
-				int node = now[0];
-				int dis = now[1];
-				
-				if(dis > distance[node]) continue;
-				
-				if (dis!=0 && node==i) {
-					res = Math.min(res, dis);
-					break;
+//		for (int i=1; i<V+1; i++) System.out.println(i + " " + Arrays.toString(g[i]));
+		
+		int res = INF;
+		
+		for (int k=1; k<V+1; k++) {
+			for (int i=1; i<V+1; i++) {
+				for (int j=1; j<V+1; j++) {
+					g[i][j] = Math.min(g[i][j], g[i][k] + g[k][j]);
 				}
-				
-				for (int[]jc : g[node]) {
-					if(distance[jc[0]] > dis + jc[1]) {
-						distance[jc[0]] = dis+jc[1];
-						que.add(new int[] {jc[0], distance[jc[0]]});
-					}
+			}
+		}
+//		for (int i=1; i<V+1; i++) System.out.println(i + " " + Arrays.toString(g[i]));
+		
+		
+		for (int i=1; i<V+1; i++) {
+			for (int j=1; j<V+1; j++) {
+				if (g[i][j] != INF && g[j][i] != INF) {
+					res = Math.min(g[i][j]+g[j][i], res);
 				}
 			}
 		}
 		
-		if(res==Integer.MAX_VALUE) System.out.println(-1);
+		
+		if(res==INF) System.out.println(-1);
 		else System.out.println(res);
 	}
 }
